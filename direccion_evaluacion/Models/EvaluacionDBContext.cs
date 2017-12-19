3103417606f120e8dc623b2063287a2f15ac0386
UTC1992 <omar.guanoluisa25@gmail.com>
2017-12-18 23:35:49 -0500

    configuracion de cadena de conexion
    1.- se creo la configuracion de la cadena de conexion para la BD Sql
    local de Visual Studio
    2.- se creo el modelo Usuario
    3.- La vista Login
    4.- La base de datos EvaluacionDireccionDB
    5.- La tabla Usuarios

---
+++ direccion_evaluacion/Models/EvaluacionDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data.Entity;
+
+namespace direccion_evaluacion.Models
+{
+    public class EvaluacionDBContext : DbContext
+    {
+        public EvaluacionDBContext()
+            :base ("DireccionEvaluacionDB")
+        {
+
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
